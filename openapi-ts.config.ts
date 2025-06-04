454a95df14b5a6bc2fab6eb51c24a8810ad589b9
Max Mueller <max97m2@gmail.com>
2025-06-04 19:56:19 +0200

    fix: use openapi json to generate client

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  input: 'https://get.heyapi.dev/hey-api/backend',
+  input: './openapi.json',
   output: 'src/client',
   plugins: [
     '@hey-api/client-axios',
     { name: '@hey-api/typescript', enums: 'typescript' },
-    { name: '@hey-api/sdk', transformer: true },
+    { name: '@hey-api/sdk', transformer: false },
     { name: '@hey-api/transformers', dates: true },
     { name: '@hey-api/schemas', type: 'json' },
   ],
